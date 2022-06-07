fb989a545587a0d87e62be3b2ed43655c800dbc1
niklas <MHLimM1414>
2022-06-07 11:17:28 +0200

    p11

--- p11/app.js
+++ p11/app.js
@@ -1,13 +1,20 @@
-const express = require("express")
-let app = express()
-let path = require('path')
-app.set('views', path.join(__dirname, 'public'))
-app.use(express.static(path.join(__dirname, 'public'), {index: 'login.html'}));
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path')
+const port = 3000;
+const users = require('./routes/users')
+const consolidate = require("consolidate")
 
 app.listen(port, function () {
     console.log("Server listening on: http://localhost:" + port)
-})
+});
 
-let users = require('./routes/users')
-app.use('/users', users)
+app.use('/', express.static(path.join(__dirname, 'public'), {index: 'login.html'}))
+
+app.use('/scripts/mustache', express.static(path.join(__dirname, 'node_modules/mustache')))
+
+app.use('/users', users);
+
+app.engine('html', consolidate.mustache)
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'))
--- p11/routes/users.js
+++ p11/routes/users.js
@@ -1,17 +1,16 @@
 const express = require('express')
-let path = require('path')
-const users = express.Router()
-
-const {Sequelize, QueryTypes} = require('sequelize');
-const {query} = require("express");
-
-users.use(express.json());
-users.use(express.text());
+const path = require('path');
+const {QueryTypes} = require('sequelize');
+const {Sequelize} = require('sequelize');
+const users = express.Router();
+const {check, validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 users.use(express.urlencoded({extended: true}))
 
 async function connectToDatabase() {
-    const sequelize = new Sequelize({
+    let sequelize = new Sequelize({
+        host: 'localhost',
         dialect: 'sqlite',
         storage: path.join(__dirname, '..', 'db', 'webengDB.db')
     });
@@ -26,24 +25,83 @@ async function connectToDatabase() {
     return sequelize;
 }
 
+async function disconnectFromDatabase(sequelize) {
+    try {
+        await sequelize.close();
+        console.log('Connection has been closed successfully.');
+    } catch (error) {
+        console.error('Unable to close connection to the database:', error);
+    }
+}
+
 users.post("/check_login", async function (req, res) {
     let sequelize = await connectToDatabase()
     let userinfos = await sequelize.query(
-        'SELECT password FROM users WHERE username = ?',
+        'SELECT password FROM users WHERE username = :username',
         {
-            replacements: [req.body.username],
+            replacements: {username: req.body.user},
             type: QueryTypes.SELECT
         }
     )
-    console.log("QUERY")
-    console.log(userinfos)
-    if (userinfos[0]) {
-        res.status(403)
+
+    let userInfos = [];
+
+    userinfos.forEach(user => {
+        userInfos.push({name: req.body.user, password: user.password})
+    });
+
+    let userAndPwCorrect = false;
+
+    for (let user of userInfos) {
+        if (bcrypt.compareSync(req.body.pw, user.password)) {
+            userAndPwCorrect = true;
+            break;
+        }
     }
-    if (userinfos["0"].username !== req.body.username || userinfos["0"].password !== req.body.password) {
-        res.status(403)
+
+    if (userAndPwCorrect) {
+        res.send("Login erfolgreich!")
+    } else {
+        res.status(403).send("Login fehlgeschlagen!")
     }
-    res.send("Login erfolgreich!")
+
+    await disconnectFromDatabase(sequelize);
+})
+
+users.post("/register", [
+        check('note').isIn(["1", "2", "3", "4", "5", "6"]),
+        check('name').escape().trim()
+    ],
+    async function (req, res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.render('register_tpl', {error_message: 'Keine Note zwischen 1 und 6 eingegeben'});
+        } else {
+            let name = req.body.user;
+            let pw = req.body.pw;
+            let note = req.body.note;
+
+            if (name === null || pw === null || note === null) res.status(403).send("Empty field!");
+
+            let sequelize = await connectToDatabase();
+
+            let hashedPW = bcrypt.hashSync(pw, 8);
+
+            await sequelize.query(
+                'INSERT INTO users (username, password, grade) VALUES (:username, :password, :grade)',
+                {
+                    replacements: {username: name, password: hashedPW, grade: note},
+                    type: QueryTypes.INSERT
+                }
+            )
+            await disconnectFromDatabase(sequelize)
+
+            res.render('register_tpl', {error_message: 'Successfully registered.'});
+        }
+    })
+
+users.get("/register", function (req, res) {
+    res.render('register_tpl')
 })
 
 module.exports = users
